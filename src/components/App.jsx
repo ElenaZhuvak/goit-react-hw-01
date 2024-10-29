406de02ef22b1c7e5dd68942be7cb681fded77c6
Elena Zhuvak <elenkazh@gmail.com>
2024-10-29 20:08:00 +0000

    adds component Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,8 @@
-// import React from 'react'
-
-const Profile = () => {
-  return (
-    <section>
-      <div>
-        <img src="" alt="" />
-        <h2>Name</h2>
-        <p>Email</p>
-        <p>Location</p>
-        <ul>
-          <li>
-            <p>Followers</p>
-            <span>11</span>
-          </li>
-          <li>
-            <p>Views</p>
-            <span>11</span>
-          </li>
-          <li>
-            <p>Likes</p>
-            <span>11</span>
-          </li>
-        </ul>
-      </div>
-    </section>
-  )
-}
+import Profile from "./Profile/Profile";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
       <Profile />
     </div>
   )
