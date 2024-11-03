11ba4ba2ca9140d3c4b4f55f9cec97012e4f7faf
Elena Zhuvak <elenkazh@gmail.com>
2024-11-03 11:46:49 +0000

    changes the path for FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import s from './FriendList.module.css';
-import FriendListItem from './FriendListItem'
+import FriendListItem from '../FriendListItem/FriendListItem'
 
 const FriendList = ({friends}) => {
     console.log(friends);
